59db85c7e55988b4e295dc9e6a375019b49e82af
bethek <kreshnikiberdemaj@gmail.com>
2017-07-06 14:54:41 +0200

    formulaire POST = OK

--- index.php
+++ index.php
@@ -1,26 +1,8 @@
 <?php
+//print_r($_POST);
 
-require_once 'app/init.php';
+?>
 
-$itemsQuery = $db->prepare("
-    SELECT id, name, done
-    FROM items
-    WHERE user = :user
-");
-
-
-$itemsQuery->execute([
-    'user' => $_SESSION['user_id']
-]);
-
-$items = $itemsQuery->rowCount() ? $itemsQuery : [];
-
-/*foreach($items as $item) {
-    echo $item['name'], '<br>';
-      print_r($item);
-}*/
-
- ?>
 
 <!DOCTYPE html>
 <html>
@@ -32,30 +14,30 @@ $items = $itemsQuery->rowCount() ? $itemsQuery : [];
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|Patrick+Hand+SC" rel="stylesheet">
   </head>
 
-
   <body>
       <div class="list">
           <h1 class="header">To do.</h1>
 
-          <?php if(!empty($items)): ?>
-          <ul class="items">
-                <?php foreach ($items as $item): ?>
-                  <li>
-                      <span class="item<?php echo $item['done'] ? ' done' : '' ?>"><?php echo $item['name']?>;</span>
-                      <?php if($item['done']): ?>
-                      <a href="#" class="done-button">Mark as done</a>
-                    <?php endif; ?>
-                  </li>
-                <?php endforeach; ?>
-          </ul>
-          <?php else: ?>
-              <p>You haven't added any items yet.</p>
-          <?php endif; ?>
 
-            <form class="item-add" action="add.php" method="POST">
-            <input type="text" name="name" placeholder="Type a new item here." class="input" autocomplete="off" required>
-            <input type="submit" value="Add" class="submit">
-          </form>
-    </div>
+          <ul class="items">
+
+                  <li>
+                      <span class="item"></span>
+
+                      <a href="#" class="done-button">Mark as done</a>
+
+                  </li>
+
+          </ul>
+
+              <p>You haven't added any items yet.</p>
+
+
+            <form class="item-add" action="" method="POST">
+            <input type="text" name="task" placeholder="Type a new item here." class="input" autocomplete="off" required>
+            <input type="submit" name="btn_ajouter" value="Ajouter" class="submit">
+            </form>
+      </div>
+
   </body>
 </html>
